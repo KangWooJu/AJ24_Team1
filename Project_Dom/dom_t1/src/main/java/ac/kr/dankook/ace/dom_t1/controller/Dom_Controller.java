f8f03e36c2f2c8136658740823401335d1496c70
Kw-jinwoo <woodykwon01@gmail.com>
2024-05-07 03:30:44 +0900

    main_page_developing1

--- Project_Dom/dom_t1/src/main/java/ac/kr/dankook/ace/dom_t1/controller/Dom_Controller.java
+++ Project_Dom/dom_t1/src/main/java/ac/kr/dankook/ace/dom_t1/controller/Dom_Controller.java
@@ -3,6 +3,7 @@ package ac.kr.dankook.ace.dom_t1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -18,6 +19,13 @@ public class Dom_Controller {
     public String Dom_register(Model model)
     {
         return "dom_register";
+    }
+
+    @PostMapping("main/search")
+    public String Dom_main_search(@ModelAttribute("searchForm") SearchForm searchForm)
+    {
+        System.out.println(searchForm.getInput());
+        return "dom_main";
     }
 
 
