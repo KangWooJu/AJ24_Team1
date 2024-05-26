a753b3fba0939af5a67d1ce2d1eb0484175240b2
Kw-jinwoo <woodykwon01@gmail.com>
2024-05-26 13:23:34 +0900

    merge start

---
+++ FrontEnd-Merge/Project_Dom/dom_t1/src/main/java/ac/kr/dankook/ace/dom_t1/controller/Dom_Controller.java
@@ -0,0 +1,75 @@
+package ac.kr.dankook.ace.dom_t1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Dom_Controller {
+
+    @GetMapping("main")
+    public String Dom_main(Model model)
+    {
+        return "dom_main";
+    }
+
+    @GetMapping("sign_in")
+    public String Dom_login(Model model)
+    {
+        model.addAttribute("loginInputForm", new LoginInputForm("", ""));
+        return "sign-in";
+    }
+
+    @GetMapping("sign_up")
+    public String Dom_registration(Model model)
+    {
+        return "sign-up";
+    }
+
+    @GetMapping("post_product")
+    public String Dom_post_product(Model model)
+    {
+        return "post-product";
+    }
+
+    @GetMapping("password_check")
+    public String Dom_password_check(Model model)
+    {
+        return "password-check";
+    }
+
+    @PostMapping("register") 
+    public String Dom_register(Model model)
+    {
+        return "dom_register";
+    }
+
+    @GetMapping("product_list")
+    public String Dom_product_list(Model model)
+    {
+        return "product-list";
+    }
+
+    @GetMapping("product_detail")
+    public String Dom_product_detail(Model model)
+    {
+        return "product-detail";
+    }
+
+    @PostMapping("main/search")
+    public String Dom_main_search(@ModelAttribute("searchForm") SearchForm searchForm)
+    {
+        System.out.println(searchForm.getInput());
+        return "dom_main";
+    }
+
+    @PostMapping("login/submit")
+    public String DOM_Login_Submit(@ModelAttribute LoginInputForm inputForm, Model model){
+        model.addAttribute("loginInputForm", new LoginInputForm("", ""));
+        System.out.println(inputForm.toString());
+        return "login_form";
+    }
+    
+}
