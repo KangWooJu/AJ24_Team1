d20b1042c4e7b2bef273e74e28f333edfc774791
Kw-jinwoo <woodykwon01@gmail.com>
2024-06-06 18:33:25 +0900

    6/6-2

---
+++ FrontEnd-Process/Project_Dom/dom_t1/src/main/java/ac/kr/dankook/ace/dom_t1/controller/WebConfig.java
@@ -0,0 +1,15 @@
+package ac.kr.dankook.ace.dom_t1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**")
+            .addResourceLocations("file:///C:/resource/");
+    }
+}
